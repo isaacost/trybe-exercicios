bad59bdae60c7b3d3612383c6944e1ef1f9bdba3
Isa Costa <isa-hcosta@hotmail.com>
2022-06-22 19:09:14 -0300

    adiciona ex2, ex3 e ex4

---
+++ exercicios/Fundamentos/Bloco-04/Dia-02/ex4.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+    soma += numbers[i];
+}
+
+let media = soma / numbers.length;
+
+if (media > 20) {
+    console.log("valor maior que 20")
+} else {
+    console.log("valor menor ou igual a 20");
+}
