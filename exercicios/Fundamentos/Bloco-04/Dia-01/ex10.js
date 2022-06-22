b2533891d42651961744c609d247e5f6d9a4316f
Isa Costa <isa-hcosta@hotmail.com>
2022-06-21 22:25:33 -0300

    adiciona exercicio 10

---
+++ exercicios/Fundamentos/Bloco-04/Dia-01/ex10.js
@@ -0,0 +1,11 @@
+const custo = 242.00;
+const venda = 373.00;
+let custoComImposto = custo * 1.20;
+let lucro = (venda - custoComImposto)*1000;
+
+if (custo < 0 || venda < 0) {
+    console.log('ERRO!');
+    break;
+} else {
+    console.log(lucro);
+}
