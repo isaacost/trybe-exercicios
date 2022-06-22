c1a2fb1bbf30d0aefdcd94cfbf506c6dfa3696e4
Isa Costa <isa-hcosta@hotmail.com>
2022-06-21 22:10:27 -0300

    adiciona exercicio 7

---
+++ exercicios/Fundamentos/Bloco-04/Dia-01/ex7.js
@@ -0,0 +1,17 @@
+let nota = 90;
+
+if (nota >= 90 && nota <= 100) {
+    console.log('A');
+} else if (nota >= 80) {
+    console.log('B');
+} else if (nota >= 70) {
+    console.log('C');
+} else if (nota >= 60) {
+    console.log('D');
+} else if (nota >= 50) {
+    console.log('E');
+} else if (nota < 50 && nota >= 0) {
+    console.log('F');
+} else {
+    console.log('ERRO!');
+}
