d9ba37aca591e26d7704d675c4e7a0f3bf02178e
Isa Costa <isa-hcosta@hotmail.com>
2022-06-24 15:22:56 -0300

    edita ex4

--- exercicios/Fundamentos/Bloco-04/Dia-03/ex4.js
+++ exercicios/Fundamentos/Bloco-04/Dia-03/ex4.js
@@ -2,16 +2,16 @@ let maiorNumeroPrimo = 0;
 let arrayPrimos = [];
 
 for (let num = 2; num <= 50; num += 1){
-    let primo = true;
+    let primo = 0;
 
     for (let div = 2; div < num; div += 1) {
         if (num % div === 0) {
-            primo = false;
+            primo += 1;
         }
 
     }
 
-    if (primo) {
+    if (primo == 0) {
         maiorNumeroPrimo = num;
         arrayPrimos.push(num);
     }
